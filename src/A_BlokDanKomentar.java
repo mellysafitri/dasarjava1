b88e9a1a6597e5e0065ebca79d8a21242aad2f3f
mellysafitri <mellysafitri056@gmail.com>
2022-03-14 15:59:33 +0700

    per

--- src/A_BlokDanKomentar.java
+++ src/A_BlokDanKomentar.java
@@ -2,7 +2,6 @@ public class A_BlokDanKomentar {
     public static void main(String[] args) {
         System.out.println("Pemrograman Berorientasi Objek dengan Java");
         //contoh komentar 1 baris
-
         /*
         contoh komentar lebih dari 1 baris
          */
