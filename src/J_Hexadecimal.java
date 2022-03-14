09dc09a64014e8af9b3fda436c2fc7fe09647733
mellysafitri <mellysafitri056@gmail.com>
2022-03-14 17:33:26 +0700

    percobaan menampilkan bilangan hexadecimal keformat decimal dijava

---
+++ src/J_Hexadecimal.java
@@ -0,0 +1,13 @@
+public class J_Hexadecimal {
+    public static void main(String[] args) {
+        int x=0x0001;
+        int y=0x7ffffff;
+        int z=0xDeadCafe;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+    }
+    /**
+     * contoh menampilkan bilangan hexadecimal keformat decimal dijava
+     */
+}
