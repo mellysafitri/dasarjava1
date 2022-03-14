3234cc608ae6fc5ecfb650e9347283ac36fbe351
mellysafitri <mellysafitri056@gmail.com>
2022-03-14 17:25:55 +0700

    percobaan menampilkan bilangan oktal oktal keformat desimal dijava

---
+++ src/I_Octal.java
@@ -0,0 +1,17 @@
+public class I_Octal {
+    public static void main(String[] args) {
+        int six=60;
+        int seven=07;
+        int eight=010;
+        int nine=011;
+        int twenty=014;
+        System.out.println("octal six ="+six);
+        System.out.println("octal seven ="+seven);
+        System.out.println("octal eight ="+eight);
+        System.out.println("octal nine="+nine);
+        System.out.println("octal twenty=+twenty");
+    }
+}
+/**
+ * contoh menampilkan bilangan oktal oktal keformat desimal dijava
+ */
