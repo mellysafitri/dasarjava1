fb9e63f2977e2579dd90f05dc1a3030e30dab78b
mellysafitri <mellysafitri056@gmail.com>
2022-03-14 16:37:20 +0700

    percobaan variabel di Java

---
+++ src/C_Variabel.java
@@ -0,0 +1,13 @@
+/**
+ * contoh penggunaan variabel di Java
+ */
+public class C_Variabel {
+    public static void main(String[] args) {
+
+            double salary;
+            long _distance;
+            boolean $flag;
+        }
+    }
+
+
