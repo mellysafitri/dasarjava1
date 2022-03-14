0f8a77dc582407b2e39ad82441c53c8087474f97
mellysafitri <mellysafitri056@gmail.com>
2022-03-14 16:37:54 +0700

    percobaan variabel GlobalLokal di java

---
+++ src/D_VariabelGlobalLokal.java
@@ -0,0 +1,19 @@
+public class D_VariabelGlobalLokal {
+
+    static int varGlobal=34;
+
+    public static void main(String[] args) {
+        varGlobal=25;
+        System.out.println("varGlobal diakses dimethod main dengan nilai ="+varGlobal);
+        double varLokal=3.14;
+        System.out.println("varLokal hanya bisa diakses di method main dengan nilai=" +varLokal);
+        aksesVariabelLokal();
+    }
+
+    public static void aksesVariabelLokal(){
+        System.out.println("varGlobal bisa diakses dimethod aksesVariabelLokal dengan nilai ="+varGlobal);
+        //varLokal = 2.5;
+        System.out.println("varLokal tidak bisa diakses di method aksesVariablLokal ini");
+
+    }
+}
