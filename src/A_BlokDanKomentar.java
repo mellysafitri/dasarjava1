b54a4b12b29ac4c2e8bce0ea9c3a8ceb27ed07c5
mellysafitri <mellysafitri056@gmail.com>
2022-03-14 15:32:11 +0700

    percobaan whitespace di Java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,15 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi Objek dengan Java");
+        //contoh komentar 1 baris
+
+        /*
+        contoh komentar lebih dari 1 baris
+         */
+
+        /**
+         * Java Doc --> baris ini untuk mendokumentasi kode program
+         */
+
+    }
+}
---
+++ src/B_BlokDanKomentar.java
@@ -0,0 +1,9 @@
+/**
+ * contoh penggunaaan whitespace di Java
+ */
+public class B_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi Objek dengan Java");
+
+    }
+}
