b0c0e455b72e142a44de1edef4296ea5e14c9ca9
mellysafitri <mellysafitri056@gmail.com>
2022-03-14 17:05:53 +0700

    percobaan mencetak nilai konstanta dijava

---
+++ src/G_Konstanta.java
@@ -0,0 +1,9 @@
+public class G_Konstanta {
+
+    public static void main(String[] args) {
+        final double PHI=3.14;
+        final double CM_PER_INCH=2.54;
+        System.out.println("nilai konstanta PHI ="+PHI);
+        System.out.println("nilai konstanta  CM_PER_INCH="+ CM_PER_INCH);
+    }
+}
