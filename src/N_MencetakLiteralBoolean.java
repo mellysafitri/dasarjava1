94e7e7c5e1c67bfd5e462655e2e1ce6d3401fb2a
PC 13 <arindahafidah@gmail.com>
2022-03-21 03:05:04 -0700

    percobaan mencetak literal boolean

---
+++ src/N_MencetakLiteralBoolean.java
@@ -0,0 +1,12 @@
+public class N_MencetakLiteralBoolean {
+        public static void main(String[] args) {
+            boolean isOpen=true;
+            boolean isClose=false;
+            System.out.println("contoh literal boolean kondisi"+isOpen);
+            System.out.println("contoh literal boolean kondisi"+isClose);
+        }
+    /**
+     * contoh mencetak literal boolean
+     */
+}
+
