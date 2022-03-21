6362fe0f97f3e1733d5e098bee0a6fa544b02c3d
PC 13 <arindahafidah@gmail.com>
2022-03-21 03:23:27 -0700

    percobaan membaca masukan dari keyboard menggunakan kelas scanner

---
+++ src/R_MembacaMasukkanDariKeyboardMenggunakanKelasScanner.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class R_MembacaMasukkanDariKeyboardMenggunakanKelasScanner {
+
+
+        public static void main(String[] args) {
+            String name=JOptionPane.showInputDialog("what is your name?");
+            System.out.println("my name is" + name);
+            System.exit(0);
+        }
+
+    /**
+     * contoh membaca masukan dari keyboard menggunakan kelas scanner
+     */
+}
