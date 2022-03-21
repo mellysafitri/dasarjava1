49e61401661e3ab3e04854c7ff5b6f55436bc7d9
PC 13 <arindahafidah@gmail.com>
2022-03-21 03:36:05 -0700

    percobaan membaca masukan dari keyboard menggunakan buffer reader

---
+++ src/S_MembacaMasukkanDariKeyboardMenggunakanBufferedReader.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+    public class S_MembacaMasukkanDariKeyboardMenggunakanBufferedReader {
+
+
+
+        public static void main(String[]args)throws IOException {
+            InputStreamReader key= new InputStreamReader(System.in);
+            BufferedReader input= new BufferedReader(key);
+            System.out.print ("What is your name?:");
+            String name = input.readLine();
+            System.out.println("My name is" + name);
+        }
+
+        /**
+         * contoh membaca masukan dari keyboard menggunakan Buffer Reader
+         */
+
+    }
+
+
